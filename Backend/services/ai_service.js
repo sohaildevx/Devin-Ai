8bf7d3b5fb777eb6baeff6cd2886a72bb02ee78e
SohailShaikh7860 <sohailshaikh6417@gmail.com>
2025-11-08 19:57:43 +0530

    modified the ai to give a proper answer

--- Backend/services/ai_service.js
+++ Backend/services/ai_service.js
@@ -1,13 +1,84 @@
-import {GoogleGenAI} from '@google/genai';
+import { GoogleGenAI } from "@google/genai";
 const GOOGLE_AI_KEY = process.env.GOOGLE_AI_KEY;
 
-const ai = new GoogleGenAI({apiKey: GOOGLE_AI_KEY});
+const ai = new GoogleGenAI({ apiKey: GOOGLE_AI_KEY });
 
 async function main(prompt) {
   const response = await ai.models.generateContent({
-    model: 'gemini-2.0-flash-001',
-    systemInstruction: `You are an expert in MERN and Development. You have an experience of 10 years in the development. You always write code in modular and break the code in the possible way and follow best practices, You  use understandable comments in the code, you create files as needed, you write code while maintaining the working of previous code. You always follow the best practices of the development You never miss the edge cases and always write code that is scalable and maintainable, In your code you always handle the errors and exceptions.`,
-    contents:prompt,
+    model: "gemini-2.0-flash-001",
+    generateConfig:{
+      responseMimeType: "application/json",
+    },
+    systemInstruction: `You are an expert in MERN and Development. You have an experience of 10 years in the development. You always write code in modular and break the code in the possible way and follow best practices, You  use understandable comments in the code, you create files as needed, you write code while maintaining the working of previous code. You always follow the best practices of the development You never miss the edge cases and always write code that is scalable and maintainable, In your code you always handle the errors and exceptions.
+    
+
+    Examples:
+    
+    <example>
+    user: "create an express server"
+    response:{
+
+    "text": "this is you filetree structure of the express server",
+    "fileTree":{
+     "app.js":{
+       "file":{
+       "content": "
+       const express = require('express');~
+
+                const app = express();
+
+
+                app.get('/', (req, res) => {
+                    res.send('Hello World!');
+                });
+
+
+                app.listen(3000, () => {
+                    console.log('Server is running on port 3000');
+                })
+                ",
+
+                "package.json":"
+                {
+                  "name": "temp-server",
+                    "version": "1.0.0",
+                    "main": "index.js",
+                    "scripts": {
+                        "test": "echo \"Error: no test specified\" && exit 1"
+                    },
+                    "keywords": [],
+                    "author": "",
+                    "license": "ISC",
+                    "description": "",
+                    "dependencies": {
+                        "express": "^4.21.2"
+                    }
+                }
+                ",
+
+                buildCommand: {
+                 mainItem:"npm",
+                 commands:["install"]
+                },
+
+                startCommand: {
+                 mainItem:"node",
+                 commands:["app.js"]
+                }
+    }
+}
+  }
+    
+    </example>
+
+    <example>
+    user:hello
+    response:{
+      "text":"hi how can I help you"
+    }
+    </example>
+    `,
+    contents: prompt,
   });
   return response.text;
 }
